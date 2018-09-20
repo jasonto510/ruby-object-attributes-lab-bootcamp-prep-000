69df5086917e18301ad1ad8085d242c0d70be152
Jason To <jasto@ucdavis.edu>
2018-09-20 05:34:15 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(Person_name)
+    @name = Person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(Person_job)
+    @job = Person_job
+  end
+
+  def job
+    @job
+  end 
+end
